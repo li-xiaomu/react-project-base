94fab7aac47767bf0aaebb230a9c81d2e8bbe670
li-xiaomu <lipl@aliyun.com>
2024-12-07 20:17:22 +0800

    优化

--- src/views/about.tsx
+++ src/views/about.tsx
@@ -1,10 +1,17 @@
-import logo from '@/assets/images/logo.svg';
+import logo from '@/assets/images/logo.svg'
 function Home() {
+  const cssobj = {
+    logo: {
+      width: '60%',
+      height: 'auto',
+    },
+  }
   return (
     <div>
       <div>关于组件{process.env.REACT_APP_API_URL}</div>
-      <img className="logo" src={logo} alt="" />
+      <img style={cssobj.logo} src={logo} alt="" />
     </div>
-  );
+  )
 }
-export default Home;
+
+export default Home
